6a92680399097e1d822627486ab656effc95be6e
Feehu <sandra.ambroziak@outlook.com>
2016-06-13 21:25:48 +0200

    Revert "dodanie resources i reszty"
    This reverts commit c739f43533749470671b0abc6fb973cf76a2e80a.

--- Object.h
+++
@@ -1,8 +0,0 @@
-#pragma once
-class Object : public PhysicsActor
-{
-public:
-	Object(float x, float y , String pathName, float sx, float sy);
-	~Object(void);
-};
-
--- README.md
+++
@@ -1,3 +0,0 @@
-# feed-pokemon
-Projekt zaliczeniowy na Sztuczną Inteligencję 2016
-Inteligentny Kelner Pikachu
